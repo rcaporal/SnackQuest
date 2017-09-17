e8aa24fbf50810bdfbea034c4a625280c5993167
Rafael Caporal <rafaelcaporal@gmail.com>
2017-09-17 20:05:54 -0300

    FireBase and ButterKnife configuration files

--- app/src/main/java/com/example/android/snackquest/MainActivity.java
+++ app/src/main/java/com/example/android/snackquest/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.android.snackquest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.register_button)
+    Button mRegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(), "TESTE DO BUTTER", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
